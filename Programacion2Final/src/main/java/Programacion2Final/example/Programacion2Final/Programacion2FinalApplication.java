34e85a5960f3f1a019ceb4c7a85aa6d602a35feb
juanB <juancruzberrios73@gmail.com>
2024-06-13 12:16:48 -0300

    primer commit

---
+++ Programacion2Final/src/main/java/Programacion2Final/example/Programacion2Final/Programacion2FinalApplication.java
@@ -0,0 +1,13 @@
+package Programacion2Final.example.Programacion2Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programacion2FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Programacion2FinalApplication.class, args);
+	}
+
+}
---
+++ Programacion2Final/src/test/java/Programacion2Final/example/Programacion2Final/Programacion2FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package Programacion2Final.example.Programacion2Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Programacion2FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
